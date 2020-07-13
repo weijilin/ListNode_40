8fecf0af3d0f7a665f91b8d3bde51c7ba0c75b97
weijilin <472442433@qq.com>
2020-07-13 23:27:56 +0800

    提交代码

--- test-7_11/test-7_11/源.cpp
+++ test-7_11/test-7_11/源.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<stdlib.h>
 #include<cstring>
+#include<string>
 using namespace std;
 # if 0
 class date {
@@ -53,6 +54,7 @@ int main() {
 }
 #endif // 0
 
+#if 0
 class A
 { public:    
 A() { ++_scount; }    
@@ -69,4 +71,46 @@ void TestA() {
 	A a3(a1);
 	cout << A::GetACount() << endl;
 	system("pause");
+}
+
+#endif
+
+
+
+
+void TestString() {
+	string str;    
+	str.push_back(' ');   // 在str后插入空格    
+	str.append("hello");  // 在str后追加一个字符"hello"    
+	str += 'b';           // 在str后追加一个字符'b'       
+	str += "it";          // 在str后追加一个字符串"it"    
+	cout<<str<<endl;    
+	cout<<str.c_str()<<endl;   // 以C语言的方式打印字符串        
+							   // 获取file的后缀    
+	string file("string.cpp");    
+	size_t pos = file.rfind('.');    
+	string suffix(file.substr(pos, file.size()-pos));    
+	cout << suffix << endl;        
+	// npos是string里面的一个静态成员变量    
+	// static const size_t npos = -1;        
+	// 取出url中的域名    
+	string url("http://www.cplusplus.com/reference/string/string/find/");    
+	cout << url << endl;    
+	size_t start = url.find("://");    
+	if (start == string::npos)    {        
+		cout << "invalid url" << endl;        
+		return;    }    
+	start += 3;    
+	size_t finish = url.find('/', start);    
+	string address = url.substr(start, finish - start); 
+
+		cout << address << endl;        // 删除url的协议前缀    
+		pos = url.find("://");    
+		url.erase(0, pos+3);    
+		cout<<url<<endl; 
+}
+int main() {
+	TestString();
+	system("pause");
+	return 0;
 }
