bc7590f52b7e2717c92bb960ad30f27a4e626c64
weijilin <472442433@qq.com>
2020-07-16 22:44:02 +0800

    提交代码

--- test-7_11/test-7_11/源.cpp
+++ test-7_11/test-7_11/源.cpp
@@ -156,7 +156,7 @@ int main() {
 
 
  #include <assert.h>
-
+#if 0
 // 注意这里namespace大家下去就不要取名为bit了，否则出去容易翻车。^^ 
 namespace bit { 
 template<class T> 
@@ -306,5 +306,53 @@ iterator erase(Iterator pos)
 	iterator _endOfStorage; // 指向存储容量的尾 
 }
 	; }
+#endif
+
+#include <vector>
+
+void PrintVector(const vector<int>& v) 
+{    // const对象使用const迭代器进行遍历打印    
+	vector<int>::const_iterator it = v.begin();    
+	while (it != v.end())    
+	{        
+		cout << *it << " ";        
+		++it;    }    
+	    cout << endl; }
+
+int main() {    // 使用push_back插入4个数据    
+	vector<int> v;    
+	v.push_back(1);    
+	v.push_back(2);    
+	v.push_back(3);    
+	v.push_back(4);
+
+	// 使用迭代器进行遍历打印    
+	vector<int>::iterator it = v.begin();    
+	while (it != v.end())    
+	{        
+		cout << *it << " ";        
+		++it;
+		}
+
+    cout << endl;
+
+	// 使用迭代器进行修改    
+	it = v.begin();    
+	while (it != v.end())    
+	{        *it *= 2;       
+	      ++it;    }
+
+    // 使用反向迭代器进行遍历再打印    
+	vector<int>::reverse_iterator rit = v.rbegin();    
+	while (rit != v.rend())    
+	{        
+		cout << *rit << " ";        
+		++rit;    }    
+	cout << endl;
+
+		PrintVector(v);
+		system("pause");
+		return 0;
+	}
 
 
